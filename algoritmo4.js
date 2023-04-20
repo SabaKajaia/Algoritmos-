ae7c95d77e4d12c7d9a99a8d8f3c819d30c9054f
Saba Kajaia <sabakajaia2@gmail.com>
2023-04-20 08:28:32 +0200

    4 corregido

--- algoritmo4.js
+++ algoritmo4.js
@@ -1,9 +1,20 @@
 /*Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!*/
 
 function smashWords(words) {
-    return words.join(" ").trim();
+    let result = '';
+    for (let word of words) {
+      if (result.length === 0) {
+        result += word;
+      } else {
+        result += ' ' + word;
+      }
+    }
+    if (result.charAt(0) === ' ') {
+      result = result.slice(1);
+    }
+    if (result.charAt(result.length - 1) === ' ') {
+      result = result.slice(0, -1);
+    }
+    return result;
   }
-
-  const words = ["hello", "world"];
-const sentence = smashWords(words);
-console.log(sentence); // "hello world"
+  
