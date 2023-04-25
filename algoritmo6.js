5f3699d5738e0b48041dda06192d1f7acde5b0fc
Saba Kajaia <sabakajaia2@gmail.com>
2023-04-25 08:28:09 +0200

    6 segunda version

--- algoritmo6.js
+++ algoritmo6.js
@@ -19,3 +19,22 @@ function sumArray(arr) {
 // La función reduce devuelve el valor final del acumulador, que es la suma de todos los elementos de la matriz convertidos a números.
 
 // Entonces, en resumen, la función sumArray() toma una matriz de valores numéricos como cadenas de texto o números, y devuelve la suma de esos valores como un número.
+
+function sumArray(arr) {
+    let sum = 0; // Se inicializa una variable 'sum' con un valor de 0
+    for (let i = 0; i < arr.length; i++) { // Se utiliza un bucle 'for' para recorrer la matriz
+      sum += Number(arr[i]); // Se agrega a la variable 'sum' cada valor de la matriz 'arr' convertido en un número usando la función 'Number()'
+    }
+    return sum; // Devuelve la suma de todos los valores como un número
+  }
+//   function sumArray(arr) declara una función llamada sumArray que toma un argumento arr, que se espera que sea una matriz de valores numéricos como cadenas de texto o números.
+  
+//   let sum = 0; inicializa una variable llamada sum con un valor de 0. Esta variable se utiliza para acumular la suma de los valores de la matriz.
+  
+//   for (let i = 0; i < arr.length; i++) { establece un bucle for que recorre los elementos de la matriz. La variable i se inicializa en 0, el bucle continúa mientras i sea menor que la longitud de la matriz arr, y se incrementa en 1 en cada iteración.
+  
+//   sum += Number(arr[i]); agrega el valor actual de la matriz arr convertido a un número a la variable sum en cada iteración del bucle. La función Number() se utiliza para convertir el valor a un número antes de sumarlo a la variable sum.
+  
+//   return sum; devuelve la variable sum que contiene la suma de todos los valores de la matriz como un número.
+  
+//   Espero que esto ayude a entender cómo funciona la función. ¡Házmelo saber si tienes alguna otra pregunta!
